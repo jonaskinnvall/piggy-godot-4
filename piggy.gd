48efe96da47f2dd78a91f56f702612dbd3cb33e4
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-20 22:34:56 +0100

    chore: add our own input map actions for movement

--- piggy.gd
+++ piggy.gd
@@ -3,12 +3,12 @@ extends Area2D
 var speed = 2
 
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		position.x -= speed
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		position.x += speed
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		position.y -= speed
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		position.y += speed
 
