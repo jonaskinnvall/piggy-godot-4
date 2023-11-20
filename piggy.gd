034b8c31d345ec80945cd1df82574d191459d85d
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-20 23:14:40 +0100

    feat: add idle and run animations and trigger on movement
     Also add delta to position calculation and increase speed

--- piggy.gd
+++ piggy.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
-var speed = 2
+var speed = 70
+@onready var animation_player = $AnimationPlayer
 
 func _process(delta):
 	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
-	position += input_vector * speed
+	position += input_vector * speed * delta
+
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		animation_player.play("run")
